99f7d7a28e17a0ae047a215f793a7acb739a19a6
akoot <akoot@akoot.me>
2022-11-16 22:08:09 -0500

    Added light theme AND alternate theme toggles.. Life is good!

--- js/main.js
+++ js/main.js
@@ -15,10 +15,22 @@ const madeWithLoveMessages = [
     "This is what an AI would create if prompted \"finest website this side of heaven\""
 ]
 
+let themeToggle
 document.addEventListener("DOMContentLoaded", onLoad)
 function onLoad() {
 
-    // Setup logo
+    // Setup theme toggle button
+    themeToggle = document.querySelector("#themeToggle")
+    themeToggle.addEventListener("click", toggleTheme)
+    toggleTheme()
+
+    // Setup alternate theme button
+    // Source: https://stackoverflow.com/questions/5033650/how-to-dynamically-remove-a-stylesheet-from-the-current-page
+    // accessed 11/16/2022
+    let logo = document.querySelector("#logo")
+    logo.addEventListener("click", alternate)
+
+
     // Source: old https://akoot.me home page (patented technology)
     let logoText = document.querySelector("#logo span")
     let logoInterval = setInterval(() => {
@@ -44,7 +56,24 @@ function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
+let lightTheme = document.getElementById("lightTheme")
 function toggleTheme() {
+    lightTheme.disabled = !lightTheme.disabled
+    if (lightTheme.disabled) {
+        altTheme.disabled = true
+        themeToggle.style.backgroundImage = "url('../assets/images/icons/moon.svg')"
+    } else {
+        themeToggle.style.backgroundImage = "url('../assets/images/icons/sun.svg')"
+    }
+}
+
+let altTheme = document.getElementById("altTheme")
+function alternate() {
+    altTheme.disabled = !altTheme.disabled
+    if (!altTheme.disabled) {
+        lightTheme.disabled = true
+        themeToggle.style.backgroundImage = "url('../assets/images/icons/moon.svg')"
+    }
 }
 
 function exit() {
