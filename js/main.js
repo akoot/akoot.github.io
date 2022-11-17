2dc310b0a20e3ce3a021e19d650c27eb34cd1c85
akoot <akoot@akoot.me>
2022-11-16 20:10:06 -0500

    Now we're talkin!

--- js/main.js
+++ js/main.js
@@ -7,8 +7,24 @@ const socials = {
     youtube: "https://youtube.com/@akoot_"
 }
 
+const madeWithLoveMessages = [
+    "Made in your walls using my bare hands",
+    "Designed in California USA, made in China, in my brain (Florida USA)",
+    "Hope you didn't forget I am in your walls, where I made this website",
+    "Inspired by the great Florida weather",
+    "This is what an AI would create if prompted \"finest website this week\""
+]
+
 document.addEventListener("DOMContentLoaded", onLoad)
 function onLoad() {
+
+    // Setup logo
+    // Source: old https://akoot.me home page (patented technology)
+    let logoText = document.querySelector("#logo span")
+    let logoInterval = setInterval(() => {
+        logoText.textContent = logoText.textContent === 'Akoot' ? 'Akoot_' : 'Akoot'
+    }, 500)
+
     // Setup socials
     document.querySelectorAll(".social-icon").forEach((el) => {
         let name = el.id
@@ -18,9 +34,20 @@ function onLoad() {
 
     // Set copyright year in footer
     const year = new Date().getFullYear();
-    document.querySelector("#year").textContent = year.toString()
+    document.querySelector("#copyright .year").textContent = year.toString()
+
+    // Mandatory "made with love" message
+    document.querySelector("#madeWithLoveOrSomething").textContent = randomElement(madeWithLoveMessages)
+}
+
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
 function toggleTheme() {
+
+}
+
+function exit() {
 
 }
